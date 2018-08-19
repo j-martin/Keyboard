86c24cbb1a2f9c158af5f732c7d5460b82714f80
Jean-Martin Archer <jm@jmartin.ca>
2018-08-18 18:10:01 -0700

    Sync

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -201,20 +201,19 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
 KEYMAPS(
 
   [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_Backtick,
+   LockLayer(NUMPAD), Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftAlt,
    Key_Backspace, Key_Delete, Key_Tab, Key_Escape,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+   Key_Slash,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus,
+   Key_Escape,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    Key_Escape, Key_Tab, Key_Enter, Key_Spacebar,
    ShiftToLayer(FUNCTION)),
-
 
   [NUMPAD] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, ___,
@@ -236,14 +235,14 @@ KEYMAPS(
    Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
+   ___, ___, ___, ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
                                Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
    Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, ___,
+  ___, ___, ___, ___,
    ___)
 
   ) // KEYMAPS(
@@ -277,6 +276,8 @@ void setup() {
 
          kaleidoscope::Qukey(0, 2, 6, Key_LeftAlt), // (ESC)
          kaleidoscope::Qukey(0, 2, 9, Key_RightAlt), // (Butterfly)
+         kaleidoscope::Qukey(2, 2, 6, Key_LeftAlt), // (ESC)
+         kaleidoscope::Qukey(2, 2, 9, Key_RightAlt), // (Butterfly)
 
          // Left thumb
          kaleidoscope::Qukey(0, 1, 7, Key_LeftShift), // (Backspace)
@@ -288,8 +289,8 @@ void setup() {
          kaleidoscope::Qukey(0, 2, 8, Key_RightGui), // (ALT)
          kaleidoscope::Qukey(0, 3, 8, Key_RightControl), // (SHIFT)
          )
-  Qukeys.setTimeout(150);
-  Qukeys.setReleaseDelay(5);
+  Qukeys.setTimeout(140);
+  Qukeys.setReleaseDelay(0);
 
   // First, call Kaleidoscope's internal setup function
   Kaleidoscope.setup();
