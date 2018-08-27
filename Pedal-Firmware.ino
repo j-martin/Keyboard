90837bd912dd2e46db658e9abb30df0e71dcc0b3
Jean-Martin Archer <jm@jmartin.ca>
2018-08-27 09:32:08 -0700

    Adding firmware for kinesis rj-11 pedals

---
+++ Pedal-Firmware.ino
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include "Keyboard.h"
+
+struct Button {
+  int pinNumber;
+  bool isActivated;
+  int keys[3];
+};
+
+Button buttons[3] = {{2, false, {KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT}},
+                     {3, false, {KEY_LEFT_SHIFT}},
+                     {4, false, {KEY_LEFT_CTRL}}};
+
+void setup() {
+  for (Button &button : buttons) {
+    pinMode(button.pinNumber, INPUT_PULLUP);
+  }
+  Keyboard.begin();
+}
+
+void loop() {
+  for (Button &button : buttons) {
+    if (!button.isActivated && digitalRead(button.pinNumber) == LOW) {
+      for (int k : button.keys) {
+        Keyboard.press(k);
+      }
+      button.isActivated = true;
+      delay(10);
+    }
+    if (button.isActivated && digitalRead(button.pinNumber) == HIGH) {
+      for (int k : button.keys) {
+        Keyboard.release(k);
+      }
+      button.isActivated = false;
+      delay(10);
+    }
+  }
+}
